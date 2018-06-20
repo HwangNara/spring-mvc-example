42279e252e8dd363d14920d407615bc52ec36c96
country <hnaras@naver.com>
2018-06-20 21:44:24 +0900

    Update MockCustomerService.java

--- src/main/java/sample/customer/biz/service/MockCustomerService.java
+++ src/main/java/sample/customer/biz/service/MockCustomerService.java
@@ -66,8 +66,8 @@ public class MockCustomerService implements CustomerService {
     public void initCustomer() {
         nextId = 1;
 
-        register(new Customer("나라", "taro@aa.bb.cc", date("19750111"), 1));
-        register(new Customer("컨츄리", "jiro@aa.bb.cc", date("19760212"), 2));
+        register(new Customer("nara", "taro@aa.bb.cc", date("19750111"), 1));
+        register(new Customer("country", "jiro@aa.bb.cc", date("19760212"), 2));
         register(new Customer("gildong", "sabc@aa.bb.cc", date("19770313"), 3));
     }
 
